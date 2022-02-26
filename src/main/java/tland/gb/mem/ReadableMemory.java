c9f2c4e793ee8dc0261d883101acad26a58c49d2
iskake <iskake@protonmail.com>
2022-02-27 00:02:47 +0100

    Implement ReadableMemory class

---
+++ src/main/java/tland/gb/mem/ReadableMemory.java
@@ -0,0 +1,30 @@
+package tland.gb.mem;
+
+/**
+ * Interface for memory that can be read from (but not necessarily writable).
+ * 
+ * @see Memory
+ */
+public interface ReadableMemory {
+    // Note: there is no (practical) use for 'write-only memory' (in the context of
+    // Game Boy memory, at least), so there is no need for a 'WritableMemory'
+    // interface.
+
+    /**
+     * Read the byte at address {@code address} in memory.
+     * 
+     * @param address The address of the byte to be read from.
+     * @return The byte at the address of {@code address}.
+     * @throws IndexOutOfBoundsException If {@code address} is outside of memory.
+     */
+    public int readByte(int address) throws IndexOutOfBoundsException;
+
+    /**
+     * Read the short (little-endian) starting at address {@code address} in memory.
+     * 
+     * @param address The address of the short to be read from.
+     * @return The byte at the address of {@code address}.
+     * @throws IndexOutOfBoundsException If {@code address} is outside of memory.
+     */
+    public int readShort(int address) throws IndexOutOfBoundsException;
+}
