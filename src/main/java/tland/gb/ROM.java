f74a2fde1e6d3dba742d47b37f189bd0ee42f31b
iskake <iskake@protonmail.com>
2022-02-27 00:02:01 +0100

    Create mem package

--- src/main/java/tland/gb/ROM.java
+++
@@ -1,9 +0,0 @@
-package tland.gb;
-
-public class ROM {
-    final byte[] romBytes;
-
-    public ROM(byte[] bytes) {
-        romBytes = bytes;
-    }
-}
---
+++ src/main/java/tland/gb/mem/Memory.java
@@ -0,0 +1,27 @@
+package tland.gb.mem;
+
+/**
+ * Interface for memory that is can be read and written to.
+ * 
+ * @see ReadableMemory
+ */
+public interface Memory extends ReadableMemory {
+    /**
+     * Write {@code value} to the byte at address {@code address} in memory.
+     * 
+     * @param address The address of the byte to be written to.
+     * @param value   The value to write.
+     * @throws IndexOutOfBoundsException If {@code address} is outside of memory.
+     */
+    public void writeByte(int address, int value) throws IndexOutOfBoundsException;
+
+    /**
+     * Write {@code value} to the short (little-endian) starting at address
+     * {@code address} in memory.
+     * 
+     * @param address The address of the short to be written to.
+     * @param value   The value to write.
+     * @throws IndexOutOfBoundsException If {@code address} is outside of memory.
+     */
+    public void writeShort(int address, int value) throws IndexOutOfBoundsException;
+}
