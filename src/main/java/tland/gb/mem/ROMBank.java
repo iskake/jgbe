7b572e6bcf996dc8d4b7bd93160f1c928b53bb24
iskake <iskake@protonmail.com>
2022-02-27 23:38:50 +0100

    Fix byte reading

--- src/main/java/tland/gb/mem/ROMBank.java
+++ src/main/java/tland/gb/mem/ROMBank.java
@@ -8,7 +8,7 @@ import tland.Bitwise;
  * ROM bank of a 'Game Boy game pak'. Bank size is 16KiB (0x4000 bytes).
  */
 public class ROMBank implements ReadableMemory {
-    final static int BANK_SIZE = 0x4000;
+    public final static int BANK_SIZE = 0x4000;
     final byte[] bytes;
 
     public ROMBank(byte[] bytes) {
@@ -17,7 +17,7 @@ public class ROMBank implements ReadableMemory {
 
     @Override
     public byte readByte(int address) {
-        address = Bitwise.intAsByte(address);
+        address = Bitwise.intAsShort(address);
         return bytes[address];
     }
 
