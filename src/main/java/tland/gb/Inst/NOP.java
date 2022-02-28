ef45c0f138b9cb58bbc69898b9a397223e2cb710
iskake <iskake@protonmail.com>
2022-02-28 19:48:11 +0100

    Documentation

--- src/main/java/tland/gb/Inst/NOP.java
+++ src/main/java/tland/gb/Inst/NOP.java
@@ -2,6 +2,11 @@ package tland.gb.Inst;
 
 import tland.gb.GameBoy;
 
+/**
+ * No operation instruction.
+ * 
+ * <p> Note: the operation still takes 
+ */
 public class NOP extends Instruction {
 
     public NOP() {
