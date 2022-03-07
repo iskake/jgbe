06aaaf49ec356e9139890bada1e5e9de9801ee0b
iskake <iskake@protonmail.com>
2022-03-07 23:12:36 +0100

    Update pom.xml

--- src/main/java/tland/gb/CPU.java
+++ src/main/java/tland/gb/CPU.java
@@ -9,7 +9,9 @@ public class CPU {
 
     public void step() {
         byte lastOpcode = gb.readNextByte();
-        System.out.printf("%02x -> %s\n", Byte.toUnsignedInt(lastOpcode), String.format(Opcodes.getOpcode(lastOpcode).getName(), gb.readMemoryAddress(gb.getPC()), gb.readMemoryAddress((short)(gb.getPC() + 1))));
+        // TODO: add size (and cycles?) constant to opcodes?
+        // (e.g. xor b -> 1 byte, ld c, $n8 -> 2 bytes, ld de, $n16 -> 3 bytes)
+        System.out.printf("%02x -> %s\n", Byte.toUnsignedInt(lastOpcode), String.format(Opcodes.getOpcode(lastOpcode).getName(), gb.readMemoryAddress(gb.getPC()), gb.readMemoryAddress((short) (gb.getPC() + 1))));
         Opcodes.getOpcode(lastOpcode).doOp(gb, Byte.toUnsignedInt(lastOpcode));
         gb.reg.printRegisters();
     }
