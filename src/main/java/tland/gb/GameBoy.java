499cfc55043ddb09727bed7232abf477eb0f2876
iskake <iskake@protonmail.com>
2022-03-07 23:06:10 +0100

    (Temp) Set registers on boot.
    Also added getPC and get/setSP methods

--- src/main/java/tland/gb/GameBoy.java
+++ src/main/java/tland/gb/GameBoy.java
@@ -1,6 +1,8 @@
 package tland.gb;
 
 import tland.Bitwise;
+import tland.gb.Registers.Flags;
+import tland.gb.Registers.RegisterIndex;
 import tland.gb.mem.CartridgeROM;
 import tland.gb.mem.MemoryMap;
 
@@ -18,12 +20,35 @@ public class GameBoy {
         memoryMap = new MemoryMap(rom);
         reg = new Registers(this);
         cpu = new CPU(this);
+
+        // TODO: use BootROM instead of hardcoded values, as this may depend on
+        // revisions. In this case, the values are for the DMG (NOT the DMG0)
         pc = Bitwise.toShort(0x100);
         sp = Bitwise.toShort(0xfffe);
+        reg.writeRegisterByte(RegisterIndex.A, 0x01);
+        reg.setFlag(Flags.Z);
+        reg.resetFlag(Flags.N);
+        reg.setFlag(Flags.H);
+        reg.setFlag(Flags.C);
+        reg.writeRegisterShort(RegisterIndex.BC, (short)0x0013);
+        reg.writeRegisterShort(RegisterIndex.DE, (short)0x00d8);
+        reg.writeRegisterShort(RegisterIndex.HL, (short)0x014d);
+    }
+
+    public short getPC() {
+        return pc;
     }
 
     public void setPC(short address) {
         pc = address;
+    }
+
+    public short getSP() {
+        return sp;
+    }
+
+    public void setSP(short value) {
+        sp = value;
     }
 
     public void run() {
