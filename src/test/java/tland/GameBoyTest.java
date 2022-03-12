37baeb870ed4c6e3b06c81528656ac49dfd87b1d
iskake <iskake@protonmail.com>
2022-03-12 23:40:06 +0100

    Move tests

--- src/test/java/tland/GameBoyTest.java
+++ src/test/java/tland/GameBoyTest.java
@@ -11,10 +11,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import tland.gb.GameBoy;
-import tland.gb.Opcodes;
 import tland.gb.Registers;
 import tland.gb.Registers.Flags;
 import tland.gb.Registers.RegisterIndex;
+import tland.gb.cpu.Opcodes;
 import tland.gb.mem.CartridgeROM;
 import tland.gb.mem.ROMBank;
 
@@ -139,30 +139,5 @@ public class GameBoyTest {
         reg.writeRegisterShort(BC, (short) 0xc100);
         loadOpcode(0x0a);
         assertEquals((byte) 0xb4, reg.readRegisterByte(A));
-    }
-
-    }
-
-    @Test
-    void bitwiseTest() {
-        assertEquals((byte) 0xa8, Bitwise.getHighByte((short) 0xa8e0));
-        assertEquals((byte) 0xe0, Bitwise.getLowByte((short) 0xa8e0));
-
-        assertEquals((short) 0x1c25, Bitwise.toShort((byte) 0x1c, (byte) 0x25));
-        assertEquals((short) 0x1c25, Bitwise.toShort(0x1c25));
-
-        assertEquals((byte) 0xce, Bitwise.toByte(0xa5ce));
-
-        assertEquals(0x56, Bitwise.intAsByte(0x123456));
-        assertEquals(0x3456, Bitwise.intAsShort(0x123456));
-
-        assertEquals((byte) 0b10110001, Bitwise.flipBit((byte) 0b10110101, 2));
-        assertEquals((byte) 0b01001000, Bitwise.setBit((byte) 0b00001000, 6));
-        assertEquals((byte) 0b11011111, Bitwise.clearBit((byte) 0b11111111, 5));
-
-        assertEquals("ff", Bitwise.toHexString((byte) 0xff));
-        assertEquals("b256", Bitwise.toHexString((short) 0xb256));
-        assertEquals("11111111", Bitwise.toBinaryString((byte) 0b11111111));
-        assertEquals("1010111110111000", Bitwise.toBinaryString((short) 0b1010111110111000));
     }
 }
