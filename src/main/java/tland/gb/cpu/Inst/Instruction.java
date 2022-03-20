ac70508631e2d6621540c663b4093116ffee43e2
iskake <iskake@protonmail.com>
2022-03-20 20:37:30 +0100

    Update Instruction class

--- src/main/java/tland/gb/cpu/Inst/Instruction.java
+++ src/main/java/tland/gb/cpu/Inst/Instruction.java
@@ -8,20 +8,28 @@ import tland.gb.GameBoy;
 public abstract class Instruction {
     private String name;
 
+    /**
+     * Instruction constructor.
+     * 
+     * @param name The name of the instruction.
+     */
     public Instruction(String name) {
         this.name = name;
     }
 
     /**
      * Do the operation of the instruction.
-     * @param gb
-     * @param opcode
+     * 
+     * @param gb     GameBoy object to read/write values.
+     * @param opcode The opcode of the instruction, used for special cases.
      */
     public abstract void doOp(GameBoy gb, int opcode);
 
     /**
      * Get the name of the instruction.
-     * @return The name of the instruction, given in constructor.
+     * 
+     * @return The name of the instruction, given in the constructor or specified
+     *         internally.
      */
     public String getName() {
         return name;
