3b3e4f2ee7cab3a7a73ee2996f5f2dddac34630d
iskake <iskake@protonmail.com>
2022-03-21 19:12:32 +0100

    Update step method

--- src/main/java/tland/gb/cpu/CPU.java
+++ src/main/java/tland/gb/cpu/CPU.java
@@ -9,13 +9,24 @@ public class CPU {
         this.gb = gameBoy;
     }
 
+    /**
+     * 'Step' the CPU forward by a single instruction.
+     */
     public void step() {
-        byte lastOpcode = gb.readNextByte();
-        // TODO: add size (and cycles?) constant to opcodes?
-        // (e.g. xor b -> 1 byte, ld c, $n8 -> 2 bytes, ld de, $n16 -> 3 bytes)
-        System.out.printf("%02x -> %s\n", Byte.toUnsignedInt(lastOpcode), String.format(Opcodes.getOpcode(lastOpcode).getName(), gb.readMemoryAddress(gb.getPC()), gb.readMemoryAddress((short) (gb.getPC() + 1))));
-        Opcodes.getOpcode(lastOpcode).doOp(gb, Byte.toUnsignedInt(lastOpcode));
-        gb.reg.printRegisters();
+        short oldPC = gb.getPC();
+
+        byte opcode = gb.readNextByte();
+        Opcodes.getOpcode(opcode).doOp(gb, Byte.toUnsignedInt(opcode));
+
+        short newPC = gb.getPC();
+
+        // Temp.
+        if (oldPC == newPC) {
+            gb.printHRAM();
+            System.out.println("\nInfinite loop! Exiting.");
+            System.exit(0);
+        }
+    }
     }
 
 }
