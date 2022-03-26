aa64298d0c6bc4462322d10ceac1fa791cac9bf8
iskake <iskake@protonmail.com>
2022-03-27 00:20:46 +0100

    Add String constructor version of IllegalInstructionException

--- src/main/java/tland/gb/cpu/Inst/IllegalInstructionException.java
+++ src/main/java/tland/gb/cpu/Inst/IllegalInstructionException.java
@@ -1,5 +1,6 @@
 package tland.gb.cpu.Inst;
 
 public class IllegalInstructionException extends RuntimeException {
-
+    public IllegalInstructionException() {}
+    public IllegalInstructionException(String msg) {}
 }
