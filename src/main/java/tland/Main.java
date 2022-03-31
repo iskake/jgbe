b17ab1d75143a8ab8fb2b283926426e6457c646a
iskake <iskake@protonmail.com>
2022-03-31 23:41:46 +0200

    Add simple GUI

--- src/main/java/tland/Main.java
+++ src/main/java/tland/Main.java
@@ -1,29 +1,12 @@
 package tland;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import tland.gb.GameBoy;
-import tland.gb.mem.CartridgeROM;
+import tland.gui.GUI;
 
 public class Main 
 {
     public static void main(String[] args)
     {
-        CartridgeROM rom;
-        try {
-            Path romFile = Paths.get("rom/test.gb");
-            rom = new CartridgeROM(Files.readAllBytes(romFile));
-            
-        } catch (IOException e) {
-            System.out.println("File not found: " + e.getMessage());
-            return;
-        }
-
-        GameBoy gb = new GameBoy(rom);
-
-        gb.run();
+        GUI gui;
+        gui = new GUI();
+        gui.showGUI();
     }
 }
---
+++ src/main/java/tland/gui/GUI.java
@@ -0,0 +1,96 @@
+package tland.gui;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import tland.gb.GameBoy;
+import tland.gb.mem.CartridgeROM;
+
+public class GUI {
+    private JFrame frame;
+    private CartridgeROM rom;
+
+    public GUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            JFrame.setDefaultLookAndFeelDecorated(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            System.err.println("Could not set look and feel. Using defaults.");
+        }
+        frame = new JFrame();
+        frame.setTitle("JGBE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(160 * 3, 144 * 3);
+    }
+
+    public void showGUI() {
+        final JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem openRom = new JMenuItem("Open ROM");
+
+        openRom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent a) {
+                JFileChooser chooser = new JFileChooser();
+
+                File currDir = new File(System.getProperty("user.dir"));
+                chooser.setCurrentDirectory(currDir);
+
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Game Boy ROM files", "gb", "gbc");
+                chooser.setFileFilter(filter);
+
+                int ret = chooser.showOpenDialog(null);
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    loadROM(chooser.getSelectedFile().getPath());
+                }
+            }
+        });
+
+        fileMenu.add(openRom);
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+    }
+
+    private void loadROM(String pathString) {
+        System.out.println(pathString);
+        Path path = Paths.get(pathString);
+        byte[] romFile;
+
+        try {
+            romFile = Files.readAllBytes(path);
+        } catch (FileNotFoundException | NoSuchFileException e) {
+            System.err.println("Could not read file " + path.getFileName() + ": file not found.");
+            return;
+        } catch (IOException e) {
+            System.err.println("Could not read file " + path.getFileName() +
+                    ": an exception occurred: " + e.toString());
+            return;
+        }
+        rom = new CartridgeROM(romFile);
+        GameBoy gb = new GameBoy(rom);
+
+        gb.runDebugger();
+
+    }
+}
