b5a40140163dd6e1112af06014ac3827047ee3c1
iskake <iskake@protonmail.com>
2022-04-04 11:19:06 +0200

    Refactor the debugger

--- src/main/java/tland/gb/Debugger.java
+++ src/main/java/tland/gb/Debugger.java
@@ -15,24 +15,28 @@ public class Debugger {
     private final GameBoy gb;
     private final CPU cpu;
 
+    private Scanner sc;
+    private boolean running;
+    private String[] input;
+    private String[] lastInput = { "c" };
+    private ArrayList<Integer> breakPoints;
+    private boolean print;
+
     public Debugger(GameBoy gb, CPU cpu) {
         this.gb = gb;
         this.cpu = cpu;
+
+        sc = new Scanner(System.in);
+        running = true;
+        breakPoints = new ArrayList<>();
+        print = false;
     }
 
     /**
-     * Run the debugger
+     * Run the debugger.
      */
     public void run() {
-        gb.reg.printRegisters();
-        cpu.printNextInstruction();
-
-        Scanner sc = new Scanner(System.in);
-        boolean running = true;
-        String[] input;
-        String[] lastInput = { "c" };
-        ArrayList<Integer> breakPoints = new ArrayList<>();
-        boolean print = false;
+        printCPUInfo();
 
         while (running) {
             System.out.print("> ");
@@ -40,110 +44,159 @@ public class Debugger {
 
             if (input[0].length() == 0) {
                 input = lastInput;
+            } else {
+                lastInput = input;
             }
-            lastInput = input;
 
             switch (input[0]) {
-                case "q":
-                    running = false;
-                    break;
-                case "s":
-                    cpu.step();
-                    gb.reg.printRegisters();
-                    cpu.printNextInstruction();
-                    break;
-                case "n":
-                    byte opcode = gb.readMemoryAddress(gb.getPC());
-                    String name = Opcodes.getOpcode(opcode).getName();
-                    cpu.step();
-                    if (name.startsWith("call") || name.startsWith("rst")) {
-                        while (true) {
-                            opcode = gb.readMemoryAddress(gb.getPC());
-                            name = Opcodes.getOpcode(opcode).getName();
-                            if (name.startsWith("ret")) {
-                                short oldPC = gb.getPC();
-                                cpu.step();
-                                short newPC = gb.getPC();
-
-                                if ((short) (oldPC + 1) != newPC) {
-                                    break;
-                                }
-                            } else {
-                                cpu.step();
-                            }
-                        }
-                    }
-                    gb.reg.printRegisters();
-                    cpu.printNextInstruction();
-                    break;
-                case "c":
-                    cpu.step();
-
-                    boolean run = true;
-                    while (run) {
-                        for (Integer breakPoint : breakPoints) {
-                            if (gb.getPC() == Bitwise.toShort(breakPoint)) {
-                                System.out.printf("Hit breakpoint at $%04x\n", breakPoint);
-                                run = false;
-                            }
-                        }
-                        if (run) {
-                            cpu.step();
-                            if (print) {
-                                gb.reg.printRegisters();
-                                cpu.printNextInstruction();
-                            }
-                        }
-                    }
-                    gb.reg.printRegisters();
-                    cpu.printNextInstruction();
-                    break;
-                case "b":
-                    if (input.length == 1) {
-                        System.out.println("Breakpoints: ");
-                        int i = 1;
-                        for (Integer b : breakPoints) {
-                            System.out.printf("%d: %04x\n", i, b);
-                            i++;
-                        }
-                        break;
-                    }
-                    try {
-                        if (input[1].charAt(0) == '$') {
-                            input[1] = input[1].replace("$", "0x");
-                        } else if (input[1].charAt(0) == '%') {
-                            input[1] = input[1].replace("%", "0b");
-                        }
-                        breakPoints.add(Integer.decode(input[1]));
-                        System.out.printf("Set breakpoint at $%04x\n", breakPoints.get(breakPoints.size() - 1));
-                    } catch (Exception e) {
-                        System.err.println("Invalid syntax. Usage: `b [ |$|0x|%|0b]{MEM_ADDR}`");
-                    }
-                    break;
-                case "d":
-                    try {
-                        if (input[1].charAt(0) == '$') {
-                            input[1] = input[1].replace("$", "0x");
-                        } else if (input[1].charAt(0) == '%') {
-                            input[1] = input[1].replace("%", "0b");
-                        }
-                        int addr = Integer.decode(input[1]);
-                        addr &= 0xfff0;
-                        gb.printMemoryRegion(addr, addr + 47);
-                    } catch (Exception e) {
-                        System.err.println("Invalid syntax. Usage: `d [ |$|0x|%|0b]{MEM_ADDR}`");
-                    }
-                    break;
-                case "p":
-                    print = !print;
-                    System.out.println("Print after every step: " + print);
-                    break;
-                default:
-                    System.err.println("Unknown command: " + input[0]);
-                    break;
+                case "q", "quit", "exit" -> running = false;
+                case "c", "continue" -> continueRunning();
+                case "s", "step" -> stepInto();
+                case "n", "next" -> stepOver();
+                case "b", "break" -> handleBreakpoints(input);
+                case "x" -> dissassemble(input);
+                case "p", "print" -> enablePrinting();
+                case "reg" -> printCPUInfo();
+                default -> System.err.println("Unknown command: " + input[0]);
             }
         }
         sc.close();
+    }
+
+    /**
+     * Print information about the CPU and registers.
+     */
+    private void printCPUInfo() {
+        gb.reg.printRegisters();
+        cpu.printNextInstruction();
+    }
+
+    /**
+     * Breakpoint handling. Handles creation of new breakpoints and
+     * 
+     * @param in
+     */
+    private void handleBreakpoints(String[] in) {
+        if (in.length == 1) {
+            System.out.println("Breakpoints: ");
+            int i = 1;
+            for (Integer b : breakPoints) {
+                System.out.printf("%d: $%04x\n", i, b);
+                i++;
+            }
+            return;
+        }
+        try {
+            if (in[1].charAt(0) == '$') {
+                in[1] = in[1].replace("$", "0x");
+            } else if (in[1].charAt(0) == '%') {
+                in[1] = in[1].replace("%", "0b");
+            }
+            breakPoints.add(Integer.decode(in[1]));
+            System.out.printf("Set breakpoint at $%04x\n", breakPoints.get(breakPoints.size() - 1));
+        } catch (Exception e) {
+            System.err.println("Invalid syntax. Usage: `b [ |$|0x|%|0b]{MEM_ADDR}`");
+        }
+    }
+
+    /**
+     * Step the program one instruction forward, including stepping into 'function'
+     * calls.
+     */
+    private void stepInto() {
+        cpu.step();
+        printCPUInfo();
+    }
+
+    /**
+     * Check if a breakpoint has been hit.
+     * 
+     * @return {@code true} if a breakpoint has been hit, {@code false} otherwise.
+     */
+    private boolean checkBreakpointHit() {
+        for (Integer breakPoint : breakPoints) {
+            if (gb.getPC() == Bitwise.toShort(breakPoint)) {
+                System.out.printf("Hit breakpoint at $%04x\n", breakPoint);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Step the program one instruction forward, stepping over 'function' calls
+     * (until a {@code ret} instruction is ran)
+     * <p>
+     * Note: if a breakpoint is hit within the function call (before a {@code ret}
+     * is executed), then the debugger will stop stepping over.
+     */
+    private void stepOver() {
+        byte opcode = gb.readMemoryAddress(gb.getPC());
+        String name = Opcodes.getOpcode(opcode).getName();
+        cpu.step();
+        if (name.startsWith("call") || name.startsWith("rst")) {
+            while (true) {
+                if (checkBreakpointHit()) {
+                    break;
+                }
+
+                opcode = gb.readMemoryAddress(gb.getPC());
+                name = Opcodes.getOpcode(opcode).getName();
+
+                if (name.startsWith("ret")) {
+                    short oldPC = gb.getPC();
+                    cpu.step();
+                    short newPC = gb.getPC();
+
+                    if ((short) (oldPC + 1) != newPC) {
+                        break;
+                    }
+                } else {
+                    cpu.step();
+                }
+            }
+        }
+        printCPUInfo();
+    }
+
+    /**
+     * Continue the execution of the program until a breakpoint is hit.
+     */
+    private void continueRunning() {
+        cpu.step();
+
+        boolean run = true;
+        while (run) {
+            if (run) {
+                run = !checkBreakpointHit();
+                cpu.step();
+                if (print) {
+                    printCPUInfo();
+                }
+            }
+        }
+        printCPUInfo();
+    }
+
+    private void dissassemble(String[] in) {
+        try {
+            if (in[1].charAt(0) == '$') {
+                in[1] = in[1].replace("$", "0x");
+            } else if (in[1].charAt(0) == '%') {
+                in[1] = in[1].replace("%", "0b");
+            }
+            int addr = Integer.decode(in[1]);
+            addr &= 0xfff0;
+            gb.printMemoryRegion(addr, addr + 47);
+        } catch (Exception e) {
+            System.err.println("Invalid syntax. Usage: `d [ |$|0x|%|0b]{MEM_ADDR}`");
+        }
+    }
+
+    private void enablePrinting() {
+        print = !print;
+        System.out.println("Print after every step: " + print);
     }
 
 }
