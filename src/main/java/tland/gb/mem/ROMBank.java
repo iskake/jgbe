c63f52dbc2ae304799f5953ab62ea185fb3bf7ff
iskake <iskake@protonmail.com>
2022-04-13 11:21:09 +0200

    Update ROMBank to be a record

--- src/main/java/tland/gb/mem/ROMBank.java
+++ src/main/java/tland/gb/mem/ROMBank.java
@@ -5,13 +5,9 @@ import tland.Bitwise;
 /**
  * ROM bank of a 'Game Boy game pak'. Bank size is 16KiB (0x4000 bytes).
  */
-public class ROMBank implements ReadableMemory {
-    public final static int BANK_SIZE = 0x4000;
-    private final byte[] bytes;
 
-    public ROMBank(byte[] bytes) {
-        this.bytes = bytes;
-    }
+public record ROMBank(byte[] bytes) implements ReadableMemory {
+    public final static int BANK_SIZE = 0x4000;
 
     @Override
     public byte readByte(int address) {
