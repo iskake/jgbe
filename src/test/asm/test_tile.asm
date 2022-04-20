4ba3034ab67903c3bc86e0188229120faf4d0452
iskake <iskake@protonmail.com>
2022-04-20 22:08:40 +0200

    Update tile test

--- src/test/asm/test_tile.asm
+++ src/test/asm/test_tile.asm
@@ -3,47 +3,37 @@ section "vlbank_interrupt", rom0[$40]
 
 include "entry.asm"
 
+def OBJ0 equ _OAMRAM
+def OBJ1 equ _OAMRAM + 4
+def OBJ2 equ _OAMRAM + 8
+
+def OBJ_Y equ 0
+def OBJ_X equ 1
+def OBJ_TILE_INDEX equ 2
+def OBJ_ATTR equ 3
+
 wait_vblank:
     ld a, [rLY]
     cp $90
     jp c, wait_vblank
 
+disable_lcd:
     ld a, 0
     ld [rLCDC], a
 
-
+clear_oam:
     ld hl, _OAMRAM
     ld bc, OAM_COUNT * 4
+    call memset
 
-clear_vram:
-    ; bc: size
-    ; hl: dest
-    ld a, 0
-    ld [hli], a
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, clear_vram
-
-
+load_tiles:
     ld de, tiles_start
     ld bc, tiles_end - tiles_start
-    ld hl, _VRAM8000
-
-copy_tiles:
-    ; bc: size
-    ; de: src start
-    ; hl: dest
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, copy_tiles
+    ld hl, _VRAM
+    call memcpy
 
 set_oam_obj0:
-    ld hl, _OAMRAM
+    ld hl, OBJ0
     ld a, (SCRN_Y / 2) + 8
     ld [hli], a
     ld a, (SCRN_X / 2) + 8
@@ -53,7 +43,7 @@ set_oam_obj0:
     ld [hli], a
 
 set_oam_obj1:
-    ld hl, _OAMRAM + 4
+    ld hl, OBJ1
     ld a, (SCRN_Y / 2) + 8
     ld [hli], a
     ld a, (SCRN_X / 2) + 8
@@ -64,7 +54,7 @@ set_oam_obj1:
     ld [hli], a
 
 set_oam_obj2:
-    ld hl, _OAMRAM + 8
+    ld hl, OBJ2
     ld a, (SCRN_Y / 2) + 8
     ld [hli], a
     ld a, (SCRN_X / 2) + 8
@@ -74,11 +64,40 @@ set_oam_obj2:
     ld a, 0
     ld [hli], a
 
+set_tilemap:
+    ld de, bg_start
+    ld bc, bg_end - bg_start
+    ld hl, _VRAM + $1000
+    call memcpy
+
+    ; set tile 1
+    ld de, tiles_start
+    ld bc, tile1_end - tiles_start
+    ld hl, _VRAM + $1000 + 16
+    call memcpy
+
+clear_tilemap:
+    ld a, 0
+    ld hl, _SCRN0
+    ld bc, _SCRN1 - _SCRN0
+    call memset
+
+    ; Set tile 1 in tilemap to tile 1
+    ld a, 1
+    ld b, 32
+    ld hl, _SCRN0
+set_line1:
+    ld [hli], a
+    dec b
+    jr nz, set_line1
+
+
 start_lcd:
-    ld a, LCDCF_ON | LCDCF_OBJON
+    ld a, LCDCF_ON | LCDCF_OBJON | LCDCF_BGON
     ld [rLCDC], a
 
     ld a, %11100100
+    ld [rBGP], a
     ld [rOBP0], a
 
 enable_interrupts:
@@ -90,17 +109,56 @@ include "done.asm"
 
 tiles_start:
     db $bd,$7e, $42,$81, $a5,$a5, $a5,$a5, $81,$81, $99,$99, $42,$81, $bd,$7e,
+tile1_end:
     db $bd,$7e, $42,$81, $81,$81, $e7,$e7, $81,$81, $99,$99, $42,$81, $bd,$7e,
     db $bd,$7e, $40,$81, $a5,$a5, $a5,$a5, $81,$81, $a5,$a5, $5a,$99, $bd,$7e,
 tiles_end:
 
+bg_start:
+    db $87,$00, $0f,$00, $1e,$00, $3c,$00, $78,$00, $f0,$00, $e1,$00, $c3,$00,
+bg_end:
+
 vblank_handle:
-    ld hl, _OAMRAM + 1
+    ld hl, OBJ0 + OBJ_X  ; hl = obj0.x
     inc [hl]
-    ld hl, _OAMRAM + 4
+    ld hl, OBJ1 + OBJ_Y  ; hl = obj1.y
     inc [hl]
-    ld hl, _OAMRAM + 8
+    ld hl, OBJ2 + OBJ_Y  ; hl = obj2.y
     inc [hl]
-    inc l
+    inc l                ; hl = obj2.x
     inc [hl]
+
+    inc c
+    bit 0, c
+    jr nz, vblank_finish
+
+    ld hl, rSCX
+    inc [hl]
+
+
+vblank_finish:
     reti
+
+memset:
+    ; bc: size
+    ; hl: dest
+    ld a, 0
+    ld [hli], a
+    dec bc
+    ld a, b
+    or a, c
+    jr nz, memset
+    ret
+
+memcpy:
+    ; bc: size
+    ; de: src start
+    ; hl: dest
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jr nz, memcpy
+    ret
