bb81a97bbe3c47b905a0d70d91f5bb04dde46b3a
iskake <iskake@protonmail.com>
2022-04-29 18:12:41 +0200

    Update BitwiseTest

--- src/test/java/tland/jgbe/BitwiseTest.java
+++ src/test/java/tland/jgbe/BitwiseTest.java
@@ -1,6 +1,8 @@
 package tland.jgbe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,8 +23,18 @@ public class BitwiseTest {
         assertEquals(0x56, Bitwise.intAsByte(0x123456));
         assertEquals(0x3456, Bitwise.intAsShort(0x123456));
 
-        assertEquals((byte) 0b10110001, Bitwise.flipBit((byte) 0b10110101, 2));
         assertEquals((byte) 0b01001000, Bitwise.setBit((byte) 0b00001000, 6));
         assertEquals((byte) 0b11011111, Bitwise.clearBit((byte) 0b11111111, 5));
+        assertEquals((byte) 0b10110001, Bitwise.flipBit((byte) 0b10110101, 2));
+        assertTrue(Bitwise.isBitSet((byte)0b1000_0000, 7));
+        assertFalse(Bitwise.isBitSet((byte)0b1111_1110, 0));
+        assertEquals((byte)0b1111_0000, Bitwise.swapBits((byte)0b0000_1111));
+
+        assertEquals(1234, Bitwise.decodeInt("1234"));
+        assertEquals(0xa5e4, Bitwise.decodeInt("$a5e4"));
+        assertEquals(0xa5e4f9, Bitwise.decodeInt("0xa5e4f9"));
+        assertEquals(0b1101, Bitwise.decodeInt("%1101"));
+        assertEquals(0b110101011011000101001, Bitwise.decodeInt("0b110101011011000101001"));
+        assertEquals(1214605601, Bitwise.decodeInt("0b01001000011001010110100100100001"));
     }
 }
