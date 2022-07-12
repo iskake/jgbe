4886b4c47aeedbcd103e68702064280f7e7d7de6
iskake <iskake@protonmail.com>
2022-07-12 17:07:14 +0200

    Update getFrame and getFrameMapped

--- core/src/main/java/iskake/jgbe/core/gb/GameBoy.java
+++ core/src/main/java/iskake/jgbe/core/gb/GameBoy.java
@@ -19,6 +19,7 @@ import iskake.jgbe.core.Bitwise;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Represents a Game Boy (model 'DMG')
@@ -288,35 +289,39 @@ public class GameBoy implements IGameBoy, GameBoyDisplayable, Runnable {
         }
     }
 
+    // TODO: replace arraylist with screen buffer
     @Override
     public byte[] getFrame() {
         byte[][] in = ppu.getFrame();
-        byte[] out = new byte[in.length * in[0].length];
+        ArrayList<Byte> list = new ArrayList<>(in.length * in[0].length);
         for (int i = 0; i < in.length; i++) {
             for (int j = 0; j < in[0].length; j++) {
-                out[(i * in.length) + j] = in[i][j];
+                list.add(/* out[(i * in.length) + j] = */ in[i][j]);
             }
+        }
+
+        byte[] out = new byte[in.length * in[0].length];
+        for (int i = 0; i < list.size(); i++) {
+            out[i] = list.get(i);
         }
         return out;
     }
 
     // ? TODO: This should probably be handled by the gui instead...
-    private final int[] COLORS_MAP = {
-            0xffffff,
-            0xaaaaaa,
-            0x555555,
-            0x000000,
+    private final byte[] COLORS_MAP = {
+            (byte)0xff,
+            (byte)0xaa,
+            (byte)0x55,
+            (byte)0x00,
     };
 
     @Override
-    public int[] getFrameMapped() {
-        byte[][] in = ppu.getFrame();
-        int[] out = new int[in.length * in[0].length];
-        for (int i = 0; i < in.length; i++) {
-            for (int j = 0; j < in[0].length; j++) {
-                // TODO: needs to change based on values in rBGP, rOBP0 and rOBP1
-                out[(i * in.length) + j] = COLORS_MAP[in[i][j]];
-            }
+    public byte[] getFrameMapped() {
+        byte[] in = getFrame();
+        byte[] out = new byte[in.length * 3];
+        for (int i = 0; i < in.length * 3; i++) {
+            // TODO: needs to change based on values in rBGP, rOBP0 and rOBP1
+            out[i] = COLORS_MAP[in[i / 3]];
         }
         return out;
     }
