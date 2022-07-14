028fcc353402201f64f6221461e9bc50cb333cb1
iskake <iskake@protonmail.com>
2022-07-15 00:35:22 +0200

    Fix (?) DAA instruction

--- core/src/main/java/iskake/jgbe/core/gb/cpu/inst/DAA.java
+++ core/src/main/java/iskake/jgbe/core/gb/cpu/inst/DAA.java
@@ -3,10 +3,9 @@ package iskake.jgbe.core.gb.cpu.inst;
 import iskake.jgbe.core.gb.IGameBoy;
 import iskake.jgbe.core.gb.Registers.Flags;
 import iskake.jgbe.core.gb.Registers.Register;
-import iskake.jgbe.core.Bitwise;
 
 /**
- * Decimal Adjust Accumulator, get BCD of accumulator. 
+ * Decimal Adjust Accumulator, get BCD of accumulator.
  * (E.g. $56 + $27 = $7d = $83 in BCD)
  * 
  * <p>
@@ -23,14 +22,17 @@ public class DAA extends Instruction {
         int value = Byte.toUnsignedInt(gb.reg().readByte(Register.A));
         if (!gb.reg().isFlagSet(Flags.N)) {
             // Adjust value for additions
-            if (gb.reg().isFlagSet(Flags.H) || (value & 0b1111) > 0x09) {
+            if (gb.reg().isFlagSet(Flags.H) || (value & 0x0f) > 0x09) {
                 // carry or overflow in lower digit
                 value += 0x06;
             }
 
-            if (gb.reg().isFlagSet(Flags.C) || value > 0x99) {
+            if (gb.reg().isFlagSet(Flags.C) || value > 0x9f) {
                 // carry or overflow in higher digit
                 value += 0x60;
+                gb.reg().setFlag(Flags.C);
+            } else {
+                gb.reg().resetFlag(Flags.C);
             }
         } else {
             // Adjust value for subtractions (note: only carry and half carry)
@@ -44,9 +46,8 @@ public class DAA extends Instruction {
         }
 
         gb.reg().writeByte(Register.A, value);
-        gb.reg().setFlagConditional(Flags.Z, Bitwise.intAsByte(value) == 0);
+        gb.reg().setFlagConditional(Flags.Z, (value & 0xff) == 0);
         gb.reg().resetFlag(Flags.H);
-        gb.reg().setFlagConditional(Flags.C, value > 0x99);
     }
 
 }
