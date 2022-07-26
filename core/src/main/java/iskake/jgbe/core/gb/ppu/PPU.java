73f74e17846520c30cee2f20b6133c2e62c48cf2
iskake <iskake@protonmail.com>
2022-07-26 23:20:40 +0200

    PPU fixes and changes: + Fix colors from getMappedFrame, so they now correspond to BGP + Added multiple buffers for BG, window, and sprites + Temp: Sprite handling + Temp: Window handling - Removed getFrame - Removed scanlineBuffer: handling moved to the 3 different buffers instead.

--- core/src/main/java/iskake/jgbe/core/gb/ppu/PPU.java
+++ core/src/main/java/iskake/jgbe/core/gb/ppu/PPU.java
@@ -6,20 +6,26 @@ import iskake.jgbe.core.gb.mem.OAM;
 import iskake.jgbe.core.gb.mem.VRAM;
 import iskake.jgbe.core.Bitwise;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Pixel Processing Unit.
  */
 public class PPU {
-    /** Color based on index. */
     public static final int LCD_SIZE_X = 160;
     public static final int LCD_SIZE_Y = 144;
+    public static final int MAX_SPRITES_ON_SCANLINE = 10;
     private final OAM oam;
     private final VRAM vram;
     private final HardwareRegisters hwreg;
     private final PPUController ppuControl;
-    private final byte[] scanlineBuf;
-    /** Buffer used to hold the raw pixels on the screen (each byte is %00-%11). */
-    private final byte[] screenBuffer;
+    /** Buffer used to hold the raw pixels on the screen (bg tilemap) (each byte is %00-%11). */
+    private final byte[] bgBuffer;
+    /** Buffer used to hold the raw pixels on the screen (window tilemap) (each byte is %00-%11). */
+    private final byte[] winBuffer;
+    /** Buffer used to hold the raw pixels on the screen (sprites) (each byte is %00-%11). */
+    private final byte[] sprBuffer;
     /** Buffer used to hold the pixels of the screen mapped to RGB values. */
     private final byte[] frameBuffer;
 
@@ -29,8 +35,9 @@ public class PPU {
         this.hwreg = hwreg;
         this.ppuControl = ppuControl;
 
-        scanlineBuf = new byte[LCD_SIZE_X];
-        screenBuffer = new byte[LCD_SIZE_Y * LCD_SIZE_X];
+        bgBuffer = new byte[LCD_SIZE_Y * LCD_SIZE_X];
+        winBuffer = new byte[LCD_SIZE_Y * LCD_SIZE_X];
+        sprBuffer = new byte[LCD_SIZE_Y * LCD_SIZE_X];
         frameBuffer = new byte[LCD_SIZE_X * LCD_SIZE_Y * 3];
     }
 
@@ -39,46 +46,110 @@ public class PPU {
      * The scanline is created from the tilemap and sprites + other hw registers.
      * 
      * @param currScanline The current scanline being fetched.
-     * @param sprites      The sprites to draw.
-     * @param tiles        The tiles to use for tilemap/sprites.
-     * @param BG           The BackGround tilemap to draw.
-     * @param window       The window tilemap to draw.
+     * @param tiles1       The tiles to use for the tilemap.
+     * @param tilesN       The tiles to use for the tilemap.
+     * @param bg           The background tilemap to draw.
      */
-    private void createScanline(int currScanline, Sprite[] sprites, Tile[] tiles, TileMap BG, TileMap window) {
+    private void createScanlineBG(int currScanline, Tile[] tilesN, Tile[] tiles1, TileMap bg) {
+        if (!ppuControl.isBGAndWindowEnabled())
+            return;
+
         int SCY = hwreg.readAsInt(HardwareRegister.SCY);
         int SCX = hwreg.readAsInt(HardwareRegister.SCX);
 
-        boolean drawWindow = false;
-        boolean drawBG = false;
-        boolean drawSprites = false;
+        int bgp = hwreg.readAsInt(HardwareRegister.BGP);
 
-        int spriteSize = ppuControl.getOBJSize();
-
-        if (ppuControl.isBGAndWindowEnabled()) {
-            drawWindow = true;
-            drawBG = true;
-        }
-        if (ppuControl.areOBJsEnabled()) {
-            drawSprites = true;
-        }
-        if (drawWindow && !ppuControl.isWindowEnabled()) {
-            drawWindow = false;
-        }
-
-        Tile[] tilemapTilesBlockN = getTilesBlockN();
-        Tile[] tilemapTilesBlock1 = getTilesBlock1();
-        // TODO: handle window/sprites/tilemap enable.
         for (int i = 0; i < LCD_SIZE_X; i++) {
             int dx = (SCX + i) & 255;
             int dy = (SCY + currScanline) & 255;
-            Tile t = BG.getTileAtCoordinate(dx, dy, tilemapTilesBlock1, tilemapTilesBlockN);
+            Tile t = bg.getTileAtCoordinate(dx, dy, tiles1, tilesN);
             t.decode();
-            scanlineBuf[i] = t.getDot(dx % 8, dy % 8);
+            int colorIndex = Byte.toUnsignedInt(t.getDot(dx % 8, dy % 8));
+            bgBuffer[currScanline * LCD_SIZE_X + i] = getDotColor(bgp, colorIndex);
+        }
+    }
+
+    private void createScanlineWIN(int currScanline, Tile[] tilesN, Tile[] tiles1, TileMap map) {
+        // Possible 'solution' to missing tiles: use invalid index (4, -1, etc.) to represent
+        // 'invisible' or no values, for use when window is not enabled (/ no tiles?)
+
+        if (!ppuControl.isBGAndWindowEnabled() || !ppuControl.isWindowEnabled())
+            return;
+
+        int wx = hwreg.readAsInt(HardwareRegister.WX); // TODO: use this to calculate dots
+        int wy = hwreg.readAsInt(HardwareRegister.WY); // TODO: use this to calculate dots
+        int bgp = hwreg.readAsInt(HardwareRegister.BGP);
+
+        for (int i = 0; i < LCD_SIZE_X; i++) {
+            int dx = i & 255;
+            int dy = currScanline & 255;
+            Tile t = map.getTileAtCoordinate(dx, dy, tiles1, tilesN);
+            t.decode();
+            byte dotCol = t.getDot(dx % 8, dy % 8);
+            winBuffer[currScanline * LCD_SIZE_X + i] = dotCol;
+        }
+    }
+
+    private void createScanlineSPR(int currScanline, Tile[] tilesN, Tile[] tiles1, Sprite[] sprites) {
+        if (!ppuControl.areOBJsEnabled())
+            return;
+
+        int SCY = hwreg.readAsInt(HardwareRegister.SCY);
+        int SCX = hwreg.readAsInt(HardwareRegister.SCX);
+
+        int obp0 = hwreg.readAsInt(HardwareRegister.OBP0);
+        int obp1 = hwreg.readAsInt(HardwareRegister.OBP1);
+
+        int spriteSize = ppuControl.getOBJSize();
+
+        List<Sprite> spritesToDraw = Arrays.stream(sprites)
+                .filter(s -> currScanline >= (Byte.toUnsignedInt(s.getYPos()) - 16)
+                        && currScanline <= (Byte.toUnsignedInt(s.getYPos()) + spriteSize - 16))
+                .toList();
+
+        // TODO! Sprites don't draw correctly
+        for (int i = 0; i < 10; i++) {
+            if (spritesToDraw.size() != 0)
+                System.out.println(spritesToDraw);
+
+            if (spritesToDraw.size() < i + 1)
+                return;
+
+            Sprite sprite = spritesToDraw.get(i);
+
+            int xPos = Byte.toUnsignedInt(sprite.getXPos());
+            int xPosScreen = (xPos - 8) % 256;
+
+            int yPos = Byte.toUnsignedInt(sprite.getYPos());
+            int yPosScreen = (yPos - spriteSize) % 256;
+
+            int tile = Byte.toUnsignedInt(sprite.getTileIndex());
+            int attr = Byte.toUnsignedInt(sprite.getAttributes());
+
+            if (xPos > 0 && xPos < LCD_SIZE_X + 8) {
+                for (int j = xPosScreen; j < (xPosScreen + 8); j++) {
+                    if (j > 0 && j < LCD_SIZE_Y) {
+                        int pal = (attr & Sprite.MASK_PALETTE_NO) == 0 ? obp0 : obp1;
+                        sprBuffer[currScanline * LCD_SIZE_X + j] = getDotColor(pal, tile);
+                    }
+                }
+                Tile t = Tile.getTileAtIndex(tile, tiles1, tilesN);
+
+            }
+        }
+
+        for (int i = 0; i < LCD_SIZE_X; i++) {
+            int dx = (SCX + i) & 255;
+            int dy = (SCY + currScanline) & 255;
+            Tile t = Tile.getTileAtIndex(sprites[0].getTileIndex(), tiles1, tilesN);
+            t.decode();
+            byte dotCol = t.getDot(dx % 8, dy % 8);
+            bgBuffer[currScanline * LCD_SIZE_X + i] = dotCol;
         }
     }
 
     /**
-     * Add the a scanline to the current frame.
+     * Add a scanline to the current frame.
      * Adds the scanline specified in the LY register, unless the value is >= 144.
      */
     public void addScanline() {
@@ -86,29 +157,31 @@ public class PPU {
 
         if (line < LCD_SIZE_Y) {
             Sprite[] sprites = getSprites();
-            Tile[] tiles = getTilesBlock1();
             TileMap BG = getBGTileMap();
             TileMap window = getWindowTileMap();
 
-            // Update the scanline buffer.
-            createScanline(line, sprites, tiles, BG, window);
+            Tile[] tilesN = getTilesBlockN();
+            Tile[] tiles1 = getTilesBlock1();
 
-            for (int i = 0; i < LCD_SIZE_X; i++) {
-                screenBuffer[line * LCD_SIZE_X + i] = scanlineBuf[i];
-            }
+            // Update the scanline buffers.
+            createScanlineBG(line, tilesN, tiles1, BG);
+//            createScanlineWIN(line, tilesN, tiles1, window); // TODO: Handle window
+            createScanlineSPR(line, tilesN, tiles1, sprites);
         }
     }
 
     /**
-     * Get the current frame (all scanlines) as a 2d byte array.
-     * 
-     * @return The current frame.
+     * Get the correct color of the specified dot, according to the palette.
+     *
+     * @param pal The palette to get the color from (either of BGP, OBP0, OPB1)
+     * @param dot The value of the dot.
+     * @return The correct color of the dot.
      */
-    public byte[] getFrame() {
-        // Note: temp.
-        return screenBuffer;
+    private byte getDotColor(int pal, int dot) {
+        return (byte) ((pal & (0b11 << (dot * 2))) >> (dot * 2));
     }
 
+    /** Color based on index. */
     private final byte[] COLORS_MAP = {
             (byte) 0xff,
             (byte) 0xaa,
@@ -117,9 +190,25 @@ public class PPU {
     };
 
     public byte[] getMappedFrame() {
-        for (int i = 0; i < screenBuffer.length * 3; i++) {
-            // TODO: needs to change based on values in rBGP, rOBP0 and rOBP1
-            frameBuffer[i] = COLORS_MAP[screenBuffer[i / 3]];
+        for (int i = 0; i < LCD_SIZE_X * LCD_SIZE_Y * 3; i += 3) {
+            byte sprColor = sprBuffer[i / 3];
+            byte winColor = winBuffer[i / 3];
+            byte bgColor = bgBuffer[i / 3];
+
+            // TODO: Actually handle window dots too.
+            if (sprColor != 0) {
+                frameBuffer[i] = COLORS_MAP[sprColor];
+                frameBuffer[i + 1] = COLORS_MAP[sprColor];
+                frameBuffer[i + 2] = COLORS_MAP[sprColor];
+//            } else if (winColor != -1) {
+//                frameBuffer[i] = COLORS_MAP[winColor];
+//                frameBuffer[i + 1] = COLORS_MAP[winColor];
+//                frameBuffer[i + 2] = COLORS_MAP[winColor];
+            } else {
+                frameBuffer[i] =     COLORS_MAP[bgColor];
+                frameBuffer[i + 1] = COLORS_MAP[bgColor];
+                frameBuffer[i + 2] = COLORS_MAP[bgColor];
+            }
         }
         return frameBuffer;
     }
@@ -145,8 +234,7 @@ public class PPU {
 
     /**
      * Get the tiles based on the 'addressing mode'.
-     * 
-     * @param tiles The tiles to get the tile array from.
+     *
      * @return The BG and Window tilemap tile data.
      */
     private Tile[] getTilesBlockN() {
